c299f1cc320f280e9535d3f3085a74b96ce0f478
Akshay Kalose <akshay@kalose.net>
2024-11-27 21:56:38 +0530

    Add Initial Minesweeper Code
    Original Source: https://github.com/ShizukuIchi/minesweeper/commit/65aded91bb7ddbf90fd8b73eb86828332d736a6f

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,78 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import React from 'react';
+import Minesweeper from './Minesweeper';
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
+    const [platform, setPlatform] = React.useState(
+        window.innerWidth <= 768 ? 'mobile' : 'desktop',
+    );
+    const [lastTouch, setLastTouch] = React.useState(new Date());
+    const [samePos, setSamePos] = React.useState(false);
+    React.useEffect(() => {
+        function onResize() {
+            if (window.innerWidth <= 768) {
+                setPlatform('mobile');
+            } else {
+                setPlatform('desktop');
+            }
+        }
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    }, []);
+    React.useEffect(() => {
+        function touchStart() {
+            setSamePos(true);
+            setLastTouch(new Date());
+        }
+        function touchMove() {
+            setSamePos(false);
+        }
+        window.addEventListener('touchstart', touchStart);
+        window.addEventListener('touchmove', touchMove);
+        return () => {
+            window.removeEventListener('touchstart', touchStart);
+            window.removeEventListener('touchmove', touchMove);
+        };
+    }, []);
     return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+        <div
+            style={
+                platform === 'desktop'
+                    ? {
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100%',
+                    }
+                    : {
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        alignItems: 'flex-start',
+                        height: '100%',
+                    }
+            }
+        >
+            <div
+                style={
+                    platform === 'desktop'
+                        ? {
+                            display: 'inline-block',
+                        }
+                        : {
+                            transform: 'scale(1.8)',
+                            transformOrigin: 'left top',
+                            display: 'inline-block',
+                        }
+                }
+            >
+                <Minesweeper
+                    defaultDifficulty="Beginner"
+                    sameTouchPos={samePos}
+                    lastTouch={lastTouch}
+                    platform={platform}
+                />
             </div>
         </div>
-    )
+    );
 }
 
 export default App
